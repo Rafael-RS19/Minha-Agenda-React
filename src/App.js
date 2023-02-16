d03932fff44073e7becc76d3e97c32c225dbbbce
Rafael Rodrigues de Sousa <rr337262@gmail.com>
2023-02-16 12:07:41 -0300

    Atualizando arquivos

--- src/App.js
+++ src/App.js
@@ -3,7 +3,28 @@ import React from "react";
 function App() {
   return (
     <div>
-      <h1>Olá Mundo!</h1>
+      <header>
+        <h1>Minha Agenda</h1>
+      </header>
+      <form className="inputs">
+        <label for="txt">Seu nome:</label><br></br>
+        <input className="inpt" type={"text"} id="txt" placeholder="Digite o seu nome aqui..."></input><br></br><br></br>
+
+        <label for="idade">Sua idade:</label><br></br>
+        <input className="inpt" type={"number"} id="idade" placeholder="Informe sua idade..."></input><br></br><br></br>
+
+        <label for="email">Email:</label><br></br>
+        <input className="inpt" type={"email"} id="email" placeholder="Informe seu email..."></input><br></br><br></br>
+
+        <label for="fone">Telefone de contato:</label><br></br>
+        <input className="inpt" type={"tel"} id="cell" placeholder="(00) 00000-0000"></input><br></br><br></br>
+
+        <label for="txttarea">Endereço:</label><br></br>
+        <textarea id="txtarea" placeholder="Informe seu endreço, rua, número, complementos etc..." cols="40" rows="10"></textarea><br></br><br></br>
+
+        ⇨<input className="button" type={"submit"} value="Guardar Dados" onClick="salvarDados()"></input>⇦
+      </form>
+     
     </div>
   );
 }
